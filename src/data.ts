08d9968f5944e8b35cdef09745bd986fe958ffa2
Martin Ansty <martin@foundationhealth.com>
2023-09-07 11:57:16 +0100

    Challenge outline

---
+++ src/data.ts
@@ -0,0 +1,8 @@
+export const routes = [
+  { airports: ["MAN", "FRA"], distance: 845 },
+  { airports: ["MAN", "JFK"], distance: 5320 },
+  { airports: ["MAN", "LAX"], distance: 8470 },
+  { airports: ["SFO", "LAX"], distance: 560 },
+  { airports: ["SFO", "FRA"], distance: 9130 },
+  { airports: ["SFO", "JFK"], distance: 4150 },
+];
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {});
+
+app.listen(3000);
