20a9c5a2c6f0491a39ab1ea57a8adb1a3e6b8f8f
Scott Arden-Jones <4737932+scanarjo@users.noreply.github.com>
2024-04-15 16:19:57 +0100

    Format directory with prettier

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,4 +1,4 @@
-import { it, expect } from "@jest/globals";
-import { BookingService } from "./app";
+import { it, expect } from '@jest/globals';
+import { BookingService } from './app';
 
-it.todo("should calculate the cost of a one-way flight");
+it.todo('should calculate the cost of a one-way flight');
